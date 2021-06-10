2eb24c6a88ebd56fd1a109f1a9a1d5236a5b5f72
zsnyder21 <64924773+zsnyder21@users.noreply.github.com>
2021-06-10 04:35:09 -0600

    Fixed an early return from occurring on empty areas. Clean data!

--- MountainCleaner.py
+++ MountainCleaner.py
@@ -1,21 +1,203 @@
 # MountainCleaner.py
 
 import json
+import re
 from bs4 import BeautifulSoup
+from itertools import chain
 from datetime import datetime
-import pyspark as ps
+# import pyspark as ps
+import os
 
-spark = ps.sql.SparkSession.builder.master("local[4]").appName("MountainCleaner").getOrCreate()
-sc = spark.sparkContext
-spark.conf.set("spark.sql.caseSensitive", "true")
+
+# spark = ps.sql.SparkSession.builder.master("local[8]").appName("MountainCleaner").getOrCreate()
+# sc = spark.sparkContext
+# spark.conf.set("spark.sql.caseSensitive", "true")
 
 
 class MountainCleaner(object):
-	def __init__(self, dataType: str, filePath):
-		self.dataType = dataType
-		self.filePath = filePath
+    def __init__(self, filePath: str, dataType: str, exportPath: str):
+        self.filePath = filePath
+        self.dataType = dataType
+        self.exportPath = exportPath
+
+    def clean(self) -> None:
+        if self.dataType.upper() == "Route".upper():
+            self.cleanRouteInfo()
+        elif self.dataType.upper() == "Area".upper():
+            self.cleanAreaInfo()
+        elif self.dataType.upper() == "Stats".upper():
+            self.cleanStatsInfo()
+        else:
+            return
+
+    def cleanAreaInfo(self) -> None:
+        file = open(self.filePath, "r", encoding="utf8")
+
+        for line in file:
+            fileContents = json.loads(line.strip())
+            areaInfo = {
+                    "AreaId": fileContents["AreaId"],
+                    "ParentAreaId": fileContents["ParentAreaId"],
+                    "URL": fileContents["URL"]
+                }
+            self.exportToJSON(areaInfo)
+        file.close()
+
+    def cleanRouteInfo(self) -> None:
+        file = open(self.filePath, "r", encoding="utf8")
+
+        for line in file:
+            fileContents = json.loads(line.strip())
+            routeId = fileContents["RouteId"]
+            parentAreaId = fileContents["ParentAreaId"]
+            routeURL = fileContents["URL"]
+
+            soup = BeautifulSoup(fileContents["HTML"], "html.parser")
+            routeTypeInfo = soup.find(class_="description-details").find("tr").text.split(":")[1].strip().split(",")
+            difficultyInfo = self.getRouteDifficulty(soup)
+            ratingInfo = soup.find(id=f"starsWithAvgText-{routeId}").text.split()[1:4:2]
+            routeName = soup.find("h1").text.strip()
+
+            routeInfo = self.curateRouteInfo(routeId, parentAreaId, routeName, routeURL, routeTypeInfo, difficultyInfo, ratingInfo)
+
+            self.exportToJSON(routeInfo)
+
+    @classmethod
+    def curateRouteInfo(cls, routeId: int, parentAreaId: int, routeName: str, routeURL: str, routeTypeInfo: list[str],
+                        difficultyInfo: list[str], ratingInfo: list[str]) -> dict:
+        routeKeys = ["RouteId", "ParentAreaId", "Name", "URL", "Difficulty", "Severity", "Type", "Height", "Pitches", "Grade",
+                     "Rating", "VoteCount"]
+        types = {"trad", "sport", "toprope", "boulder", "ice", "aid", "mixed", "alpine"}
+        severities = {"G", "PG", "PG13", "PG-13", "R", "X"}
+        routeInfo = {"RouteId": routeId, "ParentAreaId": parentAreaId, "Name": routeName, "URL": routeURL,
+                     "Severity": difficultyInfo[-1].strip() if difficultyInfo[-1].strip() in severities else None}
+
+        routeInfo["Difficulty"] = " ".join([item.strip()
+                                            for item in difficultyInfo if item.strip() != routeInfo["Severity"]])
+
+        while routeTypeInfo:
+            info = routeTypeInfo.pop(0)
+
+            if info.strip().lower() in types:
+                if "Type" in routeInfo.keys():
+                    routeInfo["Type"] += f", {info.strip()}"
+                else:
+                    routeInfo["Type"] = info.strip()
+
+            if re.match(pattern=r"\d+\sft", string=info.strip().lower()):
+                routeInfo["Height"] = info.strip()
+
+            if "Pitches".lower() in info.lower():
+                routeInfo["Pitches"] = info.strip()
+
+            if "Grade".lower() in info.lower():
+                routeInfo["Grade"] = info.strip()
+
+        routeInfo["Rating"] = float(ratingInfo[0])
+        routeInfo["VoteCount"] = int(ratingInfo[1].replace(",", ""))
+
+        return {key: routeInfo[key] if key in routeInfo.keys() else None for key in routeKeys}
+
+    @classmethod
+    def getRouteDifficulty(cls, soup: BeautifulSoup) -> list[str]:
+        difficulty = soup.find(class_="inline-block mr-2")
+        difficultyText = difficulty.text.split()
+        difficultyChildren = difficulty.findChildren("span")
+        YDS = difficulty.find(class_="rateYDS")
+
+        if YDS is not None:
+            YDS = YDS.text.split()[0]
+        else:
+            YDS = ""
+
+        if difficultyChildren is not None:
+            difficultyToRemove = set(chain(*[child.text.split() for child in difficultyChildren]))
+        else:
+            difficultyToRemove = []
+
+        return [YDS] + [word for word in difficultyText if word not in difficultyToRemove]
+
+    def cleanStatsInfo(self) -> None:
+        file = open(self.filePath, "r", encoding="utf8")
+
+        for line in file:
+            fileContents = json.loads(line.strip())
+            routeId = fileContents["RouteId"]
+            parentAreaId = fileContents["ParentAreaId"]
+            statsURL = fileContents["URL"]
+
+            soup = BeautifulSoup(fileContents["HTML"], "html.parser")
+
+            ticksTable = soup.find(
+                class_="col-lg-6 col-sm-12 col-xs-12 mt-2 max-height max-height-md-1000 max-height-xs-400")
+
+            if ticksTable is None:
+                continue
+
+            tickCount = int(re.search(pattern=r"\d+", string=ticksTable.find("h3").text).group(0))
+            ticks = ticksTable.find_all("tr")
+            for tick in ticks:
+                userPage = tick.find("a")
+                if userPage is not None:
+                    userName = userPage.text
+                    userId = int(re.search(pattern=r"\d+", string=userPage["href"]).group(0))
+                else:
+                    userName = None
+                    userId = None
+
+                # print(userPage.text, userId)
+                tickInfo = tick.find(class_="small max-height max-height-md-120 max-height-xs-120").find("div").text
+                tickInfo = [info.strip() for info in tickInfo.split("·")]
+
+                userTick = {
+                    "UserId": userId,
+                    "UserName": userName,
+                    "RouteId": routeId,
+                    "ParentAreaId": parentAreaId,
+                    "URL": statsURL,
+                    "TickDate": tickInfo[0],
+                    # "TickDatetime": datetime.strptime(tickInfo[0], "%b %d, %Y")
+                    # if tickInfo[0].lower() != "-no date-" else None,
+                    "TickInfo": None if len(tickInfo) < 2 else tickInfo[1]
+                }
+
+                self.exportToJSON(userTick)
+
+    def exportToJSON(self, data: dict) -> None:
+        file = open(self.exportPath, "a")
+
+        jsonContent = json.dumps(data, indent=None, separators=(",", ":"))
+        print(jsonContent, file=file)
+
+        file.close()
 
 
 if __name__ == "__main__":
-	routes = spark.read.json("./data/Test Data/Routes.json")
-	print(routes)
+    # cleaner = MountainCleaner("./data2/Areas.json", "Area", "./data2/Clean/Areas.json")
+    # cleaner.clean()
+
+    # cleaner = MountainCleaner("./data2/Routes.json", "Route", "./data2/Clean/Routes.json")
+    # cleaner.clean()
+
+    cleaner = MountainCleaner("./data2/Stats.json", "Stats", "./data2/Clean/Stats.json")
+    cleaner.clean()
+
+
+    # routes = spark.read.json("./data2/Routes.json")
+    # areas = spark.read.json("./data2/Areas.json")
+    # routeStats = spark.read.json("./data2/Stats.json")
+    #
+    # routes.createOrReplaceTempView("routes")
+    # areas.createOrReplaceTempView("areas")
+    # routeStats.createOrReplaceTempView("stats")
+    #
+    # # noinspection SqlDialectInspection
+    # query = """
+    # select  RouteId,
+    #         ParentAreaId,
+    #         URL
+    #     from routes
+    # """
+    # results = spark.sql(query)
+    #
+    # results.show(100, False)
--- MountainProjectScraper.py
+++ MountainProjectScraper.py
@@ -39,22 +39,18 @@ class MountainScraper(object):
 	def findSubordinates(self, parentAreas: list[dict], parentAreaId: any([None, int]) = None) -> None:
 		for area in parentAreas:
 			currentAreaId = area["AreaId"]
-			currentURL = area["URL"]
 			soup = BeautifulSoup(area["HTML"], "html.parser")
 
 			# Determine if this is an empty area
 			if "This area is empty".upper() in soup.find(class_="mp-sidebar").text.upper():
-				return
+				continue
 
 			# Determine if we have found routes yet
-			hasRoutes = "Routes".upper() in soup.find(class_="mp-sidebar").find("h3").text.upper()
+			hasRoutes = soup.find(class_="mp-sidebar").find(id="left-nav-route-table") is not None
 
 			if hasRoutes:
 				self.findRoutes(area, currentAreaId)
 			else:
-				subAreas = {subArea.text: subArea["href"] for subArea in
-							soup.find(class_="max-height max-height-md-0 max-height-xs-400")
-								.findAll("a")}
 				subAreaInfo = [
 					{
 						"AreaId": int(re.search(pattern=r"\d+", string=subArea["href"]).group(0)),
@@ -67,8 +63,6 @@ class MountainScraper(object):
 				self.exportToJSON(subAreaInfo, "Area")
 
 				self.findSubordinates(subAreaInfo, currentAreaId)
-
-			# break
 
 	def findRoutes(self, area: dict, parentAreaId: int) -> None:
 		soup = BeautifulSoup(area["HTML"], "html.parser")
@@ -89,44 +83,8 @@ class MountainScraper(object):
 		for route in routeInfo:
 			self.findRouteStats(route["RouteId"], route["URL"], route["ParentAreaId"])
 
-		return None
-
-		routes = {route.text: route["href"] for route in
-				  soup.find(class_="max-height max-height-md-0 max-height-xs-400").findAll("a") if route["href"] != "#"}
-
-		for route, url in routes.items():
-			print(route + ": " + url)
-			return None
-			routeId = int(re.search(pattern=r"\d+", string=url).group(0))
-			soup = BeautifulSoup(requests.get(url).text, "html.parser")
-
-			routeType = soup.find(class_="description-details").find("tr").text
-
-			difficultyInfo = self.getRouteDifficulty(url)  # soup.find(class_="inline-block mr-2") # .text.split()
-			print(difficultyInfo)
-
-			# print(routeDifficultyInfo)
-			routeTypeInfo = routeType.split(":")[1].strip().split(",")
-			ratingInfo = soup.find(id=f"starsWithAvgText-{routeId}").text.split()[1:4:2]
-
-	# print(self.curateRouteInfo(route, routeId, routeTypeInfo, routeDifficultyInfo, ratingInfo))
-	# grade = soup.find(class_="rateYDS").text.split()[0]
-	# safety = soup.find()
-	# print(grade)
-
-	# self.findRouteTicks(routeId, url)
-
-	# break
-
 	def findRouteStats(self, routeId: int, url: str, parentAreaId: int) -> None:
 		url = url.replace("/route/", "/route/stats/")
-		soup = BeautifulSoup(requests.get(url).text, "html.parser")
-		# ticksTable = soup.find(
-		# 	class_="col-lg-6 col-sm-12 col-xs-12 mt-2 max-height max-height-md-1000 max-height-xs-400")
-
-		# if ticksTable is None:
-		# 	return
-
 		routeStats = [
 			{
 				"RouteId": routeId,
@@ -140,109 +98,22 @@ class MountainScraper(object):
 
 		return
 
-		tickCount = int(re.search(pattern=r"\d+", string=ticksTable.find("h3").text).group(0))
-		print(f"Total ticks: {tickCount}.")
-
-		ticks = ticksTable.find_all("tr")
-		for tick in ticks:
-			userPage = tick.find("a")
-			if userPage is not None:
-				userName = userPage.text
-				userId = int(re.search(pattern=r"\d+", string=userPage["href"]).group(0))
-			else:
-				userName = None
-				userId = None
-
-			# print(userPage.text, userId)
-			tickInfo = tick.find(class_="small max-height max-height-md-120 max-height-xs-120").find("div").text
-			tickInfo = [info.strip() for info in tickInfo.split("·")]
-
-			userTick = {
-				"UserName": userName,
-				"UserId": userId,
-				"TickDate": tickInfo[0],
-				"TickDatetime": datetime.strptime(tickInfo[0], "%b %d, %Y") if tickInfo[
-																				   0].lower() != "-no date-" else None,
-				"TickInfo": None if len(tickInfo) < 2 else tickInfo[1]
-			}
-
-			print("     ", userTick)
-
-	def exportToJSON(self, data: list[dict], dataType: str) -> None:
-		jsonContent = json.dumps(data, indent=4)
-
+	@classmethod
+	def exportToJSON(cls, data: list[dict], dataType: str) -> None:
 		if dataType.upper() == "Area".upper():
-			file = open("./data/Areas.json", "a")
-			print(jsonContent, file=file)
+			file = open("./RawData/Areas.json", "a")
+		elif dataType.upper() == "Route".upper():
+			file = open("./RawData/Routes.json", "a")
+		elif dataType.upper() == "Stats".upper():
+			file = open("./RawData/Stats.json", "a")
+		else:
+			return
 
-		if dataType.upper() == "Route".upper():
-			file = open("./data/Routes.json", "a")
-			print(jsonContent, file=file)
-
-		if dataType.upper() == "Stats".upper():
-			file = open("./data/Stats.json", "a")
+		for jsonData in data:
+			jsonContent = json.dumps(jsonData, indent=None, separators=(",", ":"))
 			print(jsonContent, file=file)
 
 		file.close()
-
-	def getRouteDifficulty(self, url: str) -> str:
-		soup = BeautifulSoup(requests.get(url).text, "html.parser")
-
-	def curateRouteInfo(self, routeName: str, routeId: int, routeInfo: list[str], routeDifficulty: list[str],
-						routeRating: list[str]) -> dict:
-		routeInfoCopy = routeInfo.copy()
-		routeDifficultyCopy = routeDifficulty.copy()
-		routeRatingCopy = routeRating.copy()
-		curatedRouteInfo = {"Id": routeId, "Name": routeName}
-		routeKeys = ["Id", "Name", "Difficulty", "Severity", "Type", "Height", "Pitches", "Grade", "Rating",
-					 "VoteCount"]
-		types = {"trad", "sport", "toprope", "boulder", "ice", "aid", "mixed", "alpine"}
-		difficultyPattern = r"(?:A|C|M|AI|WI|Mod.|Steep|Snow)\d?(?:\+|-)?"
-		severityPattern = r"(G|PG|PG-?13|R|X)"
-		difficulty = set()
-
-		if len(routeDifficultyCopy) == 1:
-			curatedRouteInfo["Difficulty"] = routeDifficultyCopy[0]
-		elif len(routeDifficultyCopy) < 3:
-			if re.match(pattern=difficultyPattern, string=routeDifficultyCopy[-1]):
-				curatedRouteInfo["Difficulty"] = f"{routeDifficultyCopy[0]} {routeDifficultyCopy[-1]}"
-			elif re.match(pattern=severityPattern, string=routeDifficultyCopy[-1]):
-				curatedRouteInfo["Severity"] = routeDifficultyCopy[-1]
-		else:
-			if re.match(pattern=severityPattern, string=routeDifficultyCopy[-1]):
-				if re.match(pattern=difficultyPattern, string=routeDifficultyCopy[-2]):
-					curatedRouteInfo["Difficulty"] = f"{routeDifficultyCopy[0]} {routeDifficultyCopy[-2]}"
-				else:
-					curatedRouteInfo["Difficulty"] = routeDifficultyCopy[0]
-				curatedRouteInfo["Severity"] = routeDifficultyCopy[-1]
-			elif re.match(pattern=difficultyPattern, string=routeDifficultyCopy[-1]):
-				curatedRouteInfo["Difficulty"] = f"{routeDifficultyCopy[0]} {routeDifficultyCopy[-1]}"
-			else:
-				curatedRouteInfo["Difficulty"] = routeDifficultyCopy[0]
-
-		while routeInfoCopy:
-			info = routeInfoCopy.pop(0)
-
-			if info.strip().lower() in types:
-				if "Type" in curatedRouteInfo.keys():
-					curatedRouteInfo["Type"] += f", {info.strip()}"
-				else:
-					curatedRouteInfo["Type"] = info.strip()
-
-			if re.match(pattern=r"\d+\sft", string=info.strip().lower()):
-				curatedRouteInfo["Height"] = info.strip()
-
-			if "Pitches".lower() in info.lower():
-				curatedRouteInfo["Pitches"] = info.strip()
-
-			if "Grade".lower() in info.lower():
-				curatedRouteInfo["Grade"] = info.strip()
-
-		curatedRouteInfo["Rating"] = routeRatingCopy[0]
-		curatedRouteInfo["VoteCount"] = routeRatingCopy[1]
-
-		return {key: curatedRouteInfo[key] if key in curatedRouteInfo.keys() else None for key in routeKeys}
-
 
 if __name__ == "__main__":
 	# scraper = MountainScraper("https://www.mountainproject.com/area/113804909/southwest-face") # Trad/Aid
